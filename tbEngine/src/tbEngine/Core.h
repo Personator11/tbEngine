844454fcd044318f0aab16651cada962489160c5
Personator11 <tbutler16@claytonschools.net>
2020-06-15 16:39:06 -0500

    preprocessor stuff

--- tbEngine/src/tbEngine/Core.h
+++ tbEngine/src/tbEngine/Core.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#ifdef TB_PLATFORM_WINDOWS
+	#ifdef TB_BUILD_DLL
+		#define	TBENGINE_API __declspec(dllexport)
+	#else
+		#define TBENGINE_API __declspec(dllimport)
+	#endif
+#else
+	#error need windows
+#endif
